a61db09b2eb96a38b87f37bf2841e3fc72e208b2
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2023-02-27 08:21:00 +0700

    feat: Cập nhật slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,26 @@ overlay.addEventListener("click", ()=>{
     overlay.classList.toggle("show");
     menu.classList.toggle("nav__items--open");
 });
+
+/**
+ * Xử lý slider show
+ * Author: THINHDH (27/02/2013)
+ */
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
