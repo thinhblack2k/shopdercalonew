79b2a314f4070ad91c3be7f1a7a40f41f2548615
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2023-02-28 23:55:08 +0700

    feat: cập nhật tab ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,33 @@ function lightbox_close() {
   document.getElementById('fade').style.display = 'none';
   lightBoxVideo.pause();
 }
+
+/**
+ * Tab UI
+ * Author: THINHDH(29/02/2023)
+ */
+
+const tabs = document.querySelectorAll('.tab-item');
+const panes = document.querySelectorAll('.tab-pane');
+
+const tabActive = document.querySelector('.tab-item.active');
+const line = document.querySelector('.tabs .line');
+
+
+
+line.style.left = tabActive.offsetLeft + 'px';
+line.style.width = tabActive.offsetWidth + 'px';
+
+tabs.forEach((tab, index)=> {
+    const pane = panes[index];
+    tab.onclick = function () {
+        document.querySelector('.tab-item.active').classList.remove('active');
+        document.querySelector('.tab-pane.active').classList.remove('active');
+
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';
+
+        this.classList.add('active');
+        pane.classList.add('active');
+    }
+})
