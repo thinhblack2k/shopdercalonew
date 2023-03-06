5f6ef18db15820907325521a15e7c3fa4994c75a
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2023-03-06 15:20:31 +0700

    update: popup video

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,17 @@ overlay.addEventListener("click", ()=>{
  * Xử lý popule video
  * Author: THINHDH (28/02/2023)
  */
+$(".vpop").on('click', function(e) {
+  e.preventDefault();
+  $("#video-popup-overlay,#video-popup-iframe-container,#video-popup-container,#video-popup-close").show();
+  $("#video-popup-iframe").on('load', function() {
+    $("#video-popup-container").show();
+  });
+});
+
+$("#video-popup-close, #video-popup-overlay").on('click', function(e) {
+  $("#video-popup-iframe-container,#video-popup-container,#video-popup-close,#video-popup-overlay").hide();
+});
 
 window.document.onkeydown = function(e) {
   if (!e) {
